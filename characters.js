d0da6f5f4f08e9086c70bd701e5dc1ae506327a1
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-07-30 10:28:37 +0800

    Update characters.js

--- characters.js
+++ characters.js
@@ -1,17 +1,26 @@
 window.addEventListener("load", function () {
 
-    console.log("Page Loaded");
-
     const object = document.getElementById("bird7");
-
     const svg = object.contentDocument;
-
-    console.log(svg);
-
     const body = svg.getElementById("body");
 
-    console.log(body);
-
     body.setAttribute("fill", "#22C55E");
+
+    let scale = 1;
+    let direction = 1;
+
+    setInterval(() => {
+
+        scale += direction * 0.01;
+
+        if (scale > 1.08) direction = -1;
+        if (scale < 0.92) direction = 1;
+
+        body.setAttribute(
+            "transform",
+            `translate(50 50) scale(${scale}) translate(-50 -50)`
+        );
+
+    }, 30);
 
 });
