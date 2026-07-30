9136ed7e8794f13dda2320aac56f79700ebfeda4
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-07-30 10:11:40 +0800

    Create characters.js

---
+++ characters.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", () => {
+
+    const object = document.getElementById("bird7");
+
+    object.addEventListener("load", () => {
+
+        const svg = object.contentDocument;
+
+        const body = svg.getElementById("body");
+
+        body.setAttribute("fill", "#22C55E");
+
+        console.log("Bird loaded!");
+
+    });
+
+});
