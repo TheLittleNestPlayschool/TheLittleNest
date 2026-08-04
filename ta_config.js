d9171469035672fc4ded9f45a8bdef65226d04a8
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-08-04 13:09:33 +0800

    Update ta_config.js

--- ta_config.js
+++ ta_config.js
@@ -2,20 +2,37 @@ const API_BASE_URL =
     'https://x8ki-letl-twmt.n7.xano.io/api:EpDLPKN0';
 
 const API_URLS = {
-    getContext: `${API_BASE_URL}/ta_get_context`,
+
+    getContext:
+        `${API_BASE_URL}/ta_get_context`,
+
     determineTeacherState:
         `${API_BASE_URL}/ta_determine_teacher_state`,
+
     getSessionAttendance:
-        `${API_BASE_URL}/ta_get_session_attendance`
+        `${API_BASE_URL}/ta_get_session_attendance`,
+
+    getLocationStudents:
+        `${API_BASE_URL}/ta_get_location_students`
+
 };
 
 const APP_CONFIG = {
-    loginPage: 'ta_loginteacher.html',
-    timeZone: 'Asia/Manila'
+
+    loginPage:
+        'ta_loginteacher.html',
+
+    timeZone:
+        'Asia/Manila'
+
 };
 
 export {
+
     API_BASE_URL,
+
     API_URLS,
+
     APP_CONFIG
+
 };
