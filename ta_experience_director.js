4db9a373c747cc1ee4e4c273a988119769ae7bce
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-08-05 09:34:29 +0800

    Update ta_experience_director.js

--- ta_experience_director.js
+++ ta_experience_director.js
@@ -2,49 +2,17 @@ import {
     renderStage
 } from './ta_stage_renderer.js';
 
+import {
+    buildDirectorStage
+} from './ta_director.js';
+
 export function startTeacherExperience(){
-    const stagePlan={
-        context:null,
 
-        modules:[
-            {
-                id:'attendance',
-                state:'active'
-            },
-            {
-                id:'observations',
-                state:'collapsed'
-            },
-            {
-                id:'media',
-                state:'collapsed'
-            },
-            {
-                id:'moments',
-                state:'collapsed'
-            },
-            {
-                id:'reflection',
-                state:'collapsed'
-            },
-            {
-                id:'messages',
-                state:'collapsed'
-            },
-            {
-                id:'see_tomorrow',
-                state:'collapsed'
-            },
-            {
-                id:'head_office_messages',
-                state:'collapsed'
-            },
-            {
-                id:'teacher_information',
-                state:'collapsed'
-            }
-        ]
-    };
+    const stagePlan=
+        buildDirectorStage();
 
-    renderStage(stagePlan);
+    renderStage(
+        stagePlan
+    );
+
 }
