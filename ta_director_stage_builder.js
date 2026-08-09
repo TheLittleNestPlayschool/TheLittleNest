30ef073e79e7293377a74778d48ab3a691461cba
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-08-09 14:05:25 +0800

    Update ta_director_stage_builder.js

--- ta_director_stage_builder.js
+++ ta_director_stage_builder.js
@@ -1,3 +1,12 @@
+import{
+    getModule
+}from'./ta_module_registry.js';
+
+const STANDARD_LAYOUT_MODE=
+    'standard';
+
+const FOCUSED_LAYOUT_MODE=
+    'focused';
 
 export function buildStagePlan(
     priorityList
@@ -7,30 +16,37 @@ export function buildStagePlan(
             ?priorityList
             :[];
 
+    const modules=
+        normalizedPriorityList.map(
+            (
+                priorityItem,
+                index
+            )=>{
+                const modulePlan=
+                    normalizePriorityItem(
+                        priorityItem
+                    );
+
+                return{
+                    ...modulePlan,
+
+                    state:
+                        index===0
+                            ?'active'
+                            :'collapsed'
+                };
+            }
+        );
+
     return{
         context:null,
 
-        modules:
-            normalizedPriorityList.map(
-                (
-                    priorityItem,
-                    index
-                )=>{
-                    const modulePlan=
-                        normalizePriorityItem(
-                            priorityItem
-                        );
+        layoutMode:
+            getInitialLayoutMode(
+                modules
+            ),
 
-                    return{
-                        ...modulePlan,
-
-                        state:
-                            index===0
-                                ?'active'
-                                :'collapsed'
-                    };
-                }
-            )
+        modules
     };
 }
 
@@ -63,4 +79,33 @@ function normalizePriorityItem(
             ''
         )
     };
+}
+
+function getInitialLayoutMode(
+    modules
+){
+    const activeModule=
+        modules.find(modulePlan=>{
+            return(
+                modulePlan.state===
+                'active'
+            );
+        })||
+        null;
+
+    if(!activeModule){
+        return STANDARD_LAYOUT_MODE;
+    }
+
+    const module=
+        getModule(
+            activeModule.id
+        );
+
+    return(
+        module?.defaultStageLayout===
+        FOCUSED_LAYOUT_MODE
+    )
+        ?FOCUSED_LAYOUT_MODE
+        :STANDARD_LAYOUT_MODE;
 }
