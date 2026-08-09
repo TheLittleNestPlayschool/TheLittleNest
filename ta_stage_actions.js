9c015d64e4a54751600f4467547de75fec990661
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-08-09 14:09:15 +0800

    Update ta_stage_actions.js

--- ta_stage_actions.js
+++ ta_stage_actions.js
@@ -1,4 +1,52 @@
+import{
+    getModule
+}from'./ta_module_registry.js';
+
+const STANDARD_LAYOUT_MODE=
+    'standard';
+
+const FOCUSED_LAYOUT_MODE=
+    'focused';
+
 export function selectStageModule(
+    stagePlan,
+    moduleId
+){
+    return{
+        ...stagePlan,
+
+        layoutMode:
+            getModuleLayoutMode(
+                moduleId
+            ),
+
+        modules:
+            getUpdatedModules(
+                stagePlan,
+                moduleId
+            )
+    };
+}
+
+export function toggleStageLayout(
+    stagePlan
+){
+    const currentLayoutMode=
+        stagePlan?.layoutMode||
+        STANDARD_LAYOUT_MODE;
+
+    return{
+        ...stagePlan,
+
+        layoutMode:
+            currentLayoutMode===
+            FOCUSED_LAYOUT_MODE
+                ?STANDARD_LAYOUT_MODE
+                :FOCUSED_LAYOUT_MODE
+    };
+}
+
+function getUpdatedModules(
     stagePlan,
     moduleId
 ){
@@ -9,22 +57,35 @@ export function selectStageModule(
             ?stagePlan.modules
             :[];
 
-    return{
-        ...stagePlan,
+    return modules.map(
+        modulePlan=>{
+            return{
+                ...modulePlan,
 
-        modules:modules.map(
-            modulePlan=>{
-                return{
-                    ...modulePlan,
-
-                    state:getNextModuleState(
+                state:
+                    getNextModuleState(
                         modulePlan,
                         moduleId
                     )
-                };
-            }
-        )
-    };
+            };
+        }
+    );
+}
+
+function getModuleLayoutMode(
+    moduleId
+){
+    const module=
+        getModule(
+            moduleId
+        );
+
+    return(
+        module?.defaultStageLayout===
+        FOCUSED_LAYOUT_MODE
+    )
+        ?FOCUSED_LAYOUT_MODE
+        :STANDARD_LAYOUT_MODE;
 }
 
 function getNextModuleState(
