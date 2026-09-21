a9be661703b0848a71f525bbfd8f3ba80cbe7ac4
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-09-21 09:31:32 +0800

    Update enrollment Xano URL

--- ta_enrollment.js
+++ ta_enrollment.js
@@ -4,7 +4,7 @@ import{
 }from'./ta_ui.js';
 
 const ENROLLMENT_API=
-    'https://x8ki-letl-twmt.n7.xano.io/api:EpDLPKN0/ta_enrollment';
+    'https://x58r-xped-p4y6.n7e.xano.io/api:EpDLPKN0/ta_enrollment';
 
 /*==================================================
   Enrollment Module
